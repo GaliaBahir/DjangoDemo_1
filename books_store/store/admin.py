6aa7f9cf123c8e75594bfb7d6e099e1facb48b78
GaliaBahir <galiab@codevalue.com>
2022-11-24 00:02:25 +0200

    Fixed admin.py

--- books_store/store/admin.py
+++ books_store/store/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'price']
