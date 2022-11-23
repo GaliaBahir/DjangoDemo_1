41ae66b4e9367000b7da341a8210f9510511aade
GaliaBahir <galiab@codevalue.com>
2022-11-23 23:45:31 +0200

    Adding Product model and register it to admin

--- books_store/store/models.py
+++ books_store/store/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default='author')
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
