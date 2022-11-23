139a00044a686da90194ee365d3220167bcffb72
GaliaBahir <galiab@codevalue.com>
2022-11-24 00:00:22 +0200

    Preparing for demo migrations

--- books_store/store/admin.py
+++ books_store/store/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Product)
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'price']
--- books_store/store/models.py
+++ books_store/store/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+# class Category(models.Model):
+#     name = models.CharField(max_length=250,db_index=True)
+
 class Product(models.Model):
+    #category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default='author')
     description = models.TextField(blank=True)
