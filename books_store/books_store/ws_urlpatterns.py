a4dc6eb61d660fb700feded1b5deb922ba73d643
GaliaBahir <galiab@codevalue.com>
2022-11-27 08:34:58 +0200

    Moved views and consumers to websockets

--- books_store/books_store/ws_urlpatterns.py
+++ books_store/books_store/ws_urlpatterns.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .consumers import NumberGenerator
+from websockets.consumers import NumberGenerator
 
 ws_urlpatterns = [
     path('ws/', NumberGenerator.as_asgi(), name='number-generator')
