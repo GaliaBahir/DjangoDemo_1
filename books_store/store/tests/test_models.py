a43b392db5eae4ffc1b718d8801ebd20ec74f9c4
GaliaBahir <galiab@codevalue.com>
2022-11-27 16:42:49 +0200

    fixed test

--- books_store/store/tests/test_models.py
+++ books_store/store/tests/test_models.py
@@ -5,8 +5,8 @@ from store.models import Product
 
 class TestProductsModel(TestCase):
     def setUp(self):
-        self.data1 = Product.objects.create(title='django beginners', price='20.00')
-        self.data2 = Product.objects.create(title='django advanced', price='30.00')
+        self.data1 = Product.objects.create(title='SW Alorithms', price='20.00')
+        self.data2 = Product.objects.create(title='web applications tutorial', price='30.00')
 
     def test_products_model_entry(self):
         """
@@ -14,4 +14,4 @@ class TestProductsModel(TestCase):
         """
         data = self.data1
         self.assertTrue(isinstance(data, Product))
-        self.assertEqual(data.title, 'django beginners')
+        self.assertEqual(data.title, 'SW Alorithms')
